70f6bd909d45fe3e23a03f98347cc0afd3877668
Anas Alhmoud <anas.alhmoud@tuwaiq.edu.sa>
2021-04-29 18:24:21 +0300

    Add project files.

---
+++ WinFormsApp2/Form1.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WinFormsApp2
+{
+    public partial class Form1 : Form
+    {
+        public class Circle
+        {
+            public int x;
+            public int y;
+            public int width;
+            public int height;
+
+            public Circle(int x, int y, int width, int height)
+            {
+                this.x = x;
+                this.y = y;
+                this.width = width;
+                this.height = height;
+            }
+
+            public Circle draw()
+            {
+                return this;
+            }
+            public Circle resize(string dir, int x, int y)
+            {
+                return this;
+            }
+            public Circle move(int dx, int dy, int x, int y)
+            {
+                // calc shape position
+                this.x = x - dx;
+                this.y = y - dy;
+
+                return this;
+            }
+        }
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        public Circle mainShape;
+        public Circle selected;
+        int dx;
+        int dy;
+        bool userIsHolding = false;
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            mainShape = new Circle(100, 200, 300, 300);
+        }
+
+        private void Form1_Paint(object sender, PaintEventArgs e)
+        {
+            var g = e.Graphics;
+
+            g.DrawEllipse(new Pen(Brushes.Black, 3), mainShape.x, mainShape.y, mainShape.width, mainShape.height);
+        }
+
+        private void Form1_MouseDown(object sender, MouseEventArgs e)
+        {
+            userIsHolding = true;
+            // TODO: Implement resize
+
+            // loop throw all shapes
+
+            Rectangle rec = new Rectangle(mainShape.x, mainShape.y, mainShape.width, mainShape.height);
+
+            if (rec.Contains(e.X, e.Y))
+            {
+                // calc d
+                dx = e.X - rec.X;
+                dy = e.Y - rec.Y;
+
+                selected = mainShape;
+            }
+        }
+
+        private void Form1_MouseUp(object sender, MouseEventArgs e)
+        {
+            selected = null;
+            userIsHolding = false;
+        }
+
+        private void Form1_MouseMove(object sender, MouseEventArgs e)
+        {
+            if(selected != null && userIsHolding)
+            {
+                selected.move(dx, dy, e.X, e.Y);
+
+                Invalidate();
+            }
+        }
+    }
+}
